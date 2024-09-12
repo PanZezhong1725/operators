28a8554b013316af60bb2903e3db3fd1a58e745e
xgqdut2016 <kenan_gewei@163.com>
2024-09-12 14:59:52 +0800

    success bang rearrange

---
+++ src/ops/rearrange/bang/rearrange_bang.cc
@@ -0,0 +1,64 @@
+#include "rearrange_bang.h"
+#include "../../../devices/bang/common_bang.h"
+#include "../../utils.h"
+#include <numeric>
+
+infiniopStatus_t bangCreateRearrangeDescriptor(BangHandle_t handle,
+                                               RearrangeBangDescriptor_t *desc_ptr,
+                                               infiniopTensorDescriptor_t dst,
+                                               infiniopTensorDescriptor_t src) {
+    if (!dtype_eq(dst->dt, src->dt)) {
+        return STATUS_BAD_TENSOR_DTYPE;
+    }
+    if (dst->ndim != src->ndim || dst->ndim < 2) {
+        return STATUS_BAD_TENSOR_SHAPE;
+    }
+    auto ndim = dst->ndim;
+    for (size_t i = 0; i < ndim; ++i) {
+        if (dst->shape[i] != src->shape[i]) {
+            return STATUS_BAD_TENSOR_SHAPE;
+        }
+    }
+    if (dst->strides[ndim - 1] != 1 || src->strides[ndim - 1] != 1) {
+        return STATUS_BAD_TENSOR_STRIDES;
+    }
+    unsigned int r = 0;
+    if (ndim == 2) {
+        r = dst->shape[0];
+    } else if (ndim == 3) {
+        r = dst->shape[0] * dst->shape[1];
+    } else {
+        for (size_t i = ndim - 3; i >= 1; --i) {
+            if (static_cast<uint64_t>(dst->shape[i]) * static_cast<uint64_t>(dst->strides[i]) != static_cast<uint64_t>(dst->strides[i - 1]) ||
+                static_cast<uint64_t>(src->shape[i]) * static_cast<uint64_t>(src->strides[i]) != static_cast<uint64_t>(src->strides[i - 1])) {
+                return STATUS_BAD_TENSOR_STRIDES;
+            }
+        }
+        r = std::accumulate(dst->shape, dst->shape + ndim - 1, 1, std::multiplies<unsigned int>());
+    }
+
+    uint64_t *mlu_shape_dst, *mlu_shape_src;
+    int64_t *mlu_strides_dst, *mlu_strides_src;
+    CNRT_CHECK(cnrtMalloc((void **) &mlu_shape_dst, ndim * sizeof(uint64_t)));
+    CNRT_CHECK(cnrtMalloc((void **) &mlu_shape_src, ndim * sizeof(uint64_t)));
+    CNRT_CHECK(cnrtMalloc((void **) &mlu_strides_dst, ndim * sizeof(int64_t)));
+    CNRT_CHECK(cnrtMalloc((void **) &mlu_strides_src, ndim * sizeof(int64_t)));
+
+    CNRT_CHECK(cnrtMemcpy(mlu_shape_dst, dst->shape, ndim * sizeof(uint64_t), cnrtMemcpyHostToDev));
+    CNRT_CHECK(cnrtMemcpy(mlu_shape_src, src->shape, ndim * sizeof(uint64_t), cnrtMemcpyHostToDev));
+    CNRT_CHECK(cnrtMemcpy(mlu_strides_dst, dst->strides, ndim * sizeof(int64_t), cnrtMemcpyHostToDev));
+    CNRT_CHECK(cnrtMemcpy(mlu_strides_src, src->strides, ndim * sizeof(int64_t), cnrtMemcpyHostToDev));
+    *desc_ptr = new RearrangeBangDescriptor{
+        handle->device,
+        handle->device_id,
+        dst->dt,
+        r,
+        ndim,
+        mlu_shape_dst, mlu_shape_src,
+        mlu_strides_dst, mlu_strides_src};
+    return STATUS_SUCCESS;
+}
+infiniopStatus_t bangDestroyRearrangeDescriptor(RearrangeBangDescriptor_t desc) {
+    delete desc;
+    return STATUS_SUCCESS;
+}
---
+++ src/ops/rearrange/bang/rearrange_bang.h
@@ -0,0 +1,32 @@
+#ifndef __BANG_REARRANGE_H__
+#define __BANG_REARRANGE_H__
+
+#include "../../../devices/bang/bang_handle.h"
+#include "operators.h"
+
+struct RearrangeBangDescriptor {
+    Device device;
+    int device_id;
+    DT dtype;
+    uint64_t r;
+    uint64_t ndim;
+    uint64_t *mlu_shape_dst, *mlu_shape_src;
+    int64_t *mlu_strides_dst, *mlu_strides_src;
+};
+
+typedef struct RearrangeBangDescriptor *RearrangeBangDescriptor_t;
+
+infiniopStatus_t bangCreateRearrangeDescriptor(BangHandle_t handle,
+                                               RearrangeBangDescriptor_t *desc_ptr,
+                                               infiniopTensorDescriptor_t dst,
+                                               infiniopTensorDescriptor_t src);
+
+infiniopStatus_t bangRearrange(RearrangeBangDescriptor_t desc,
+                               void *dst,
+                               void *src,
+                               void *stream);
+
+infiniopStatus_t bangDestroyRearrangeDescriptor(RearrangeBangDescriptor_t desc);
+
+
+#endif// __BANG_REARRANGE_H__
